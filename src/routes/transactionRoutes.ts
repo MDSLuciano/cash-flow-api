cb28c239d3909466d8be8ac7a236a2616ccb6828
Luciano Machado <lucianoo_machado@hotmail.com>
2025-03-31 07:26:51 -0300

    feat: adding the database to the routes

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -1,21 +1,19 @@
 import { FastifyInstance } from 'fastify';
 import {z} from 'zod';
+import { prisma } from '../lib/prisma';
+import { TransactionCategory, TransactionPaymentMethod, TransactionType } from '@prisma/client';
 
-
-type TransactionCategory = 'HOUSING' | 'TRANSPORTATION' | 'FOOD' | 'ENTERTAINMENT' | 'HEALTH' | 'UTILITY' | 'SALARY' | 'EDUCATION' | 'OTHER';
-type TransactionPaymentMethod = 'OTHER' | 'CREDIT_CARD' | 'DEBIT_CARD' | 'BANK_TRANSFER' | 'BANK_SLIP' | 'CASH' | 'PIX';
 interface Transaction {
 	id: number
 	title: string
 	amount: number
-	type: 'credit' | 'debit'
+	type: TransactionType
 	category: TransactionCategory
 	paymentMethod: TransactionPaymentMethod
 }
 
 const transactions: Transaction[] = [];
 
-let currentId = 0
 
 export async function transactionRoutes(app: FastifyInstance){
 	// Responsável por criar uma transação.
@@ -34,25 +32,30 @@ export async function transactionRoutes(app: FastifyInstance){
 		const { title, amount, type, category, paymentMethod } = createTransactionBodySchema.parse(request.body)
 
 		// Adiciona a nova transação ao array de transações
-		const newTransaction: Transaction = {
-			id: ++currentId,
-			title,
-			amount,
-			type,
-			category,
-			paymentMethod
-		}
-		transactions.push(newTransaction)
+		await prisma.transaction.create({
+			data:{
+				title,
+				amount,
+				type,
+				category,
+				paymentMethod
+			}
+		})
 
 		// Retorna uma resposta com o código 201 (created)
-    reply.status(201).send()
+    	reply.status(201).send()
     })
 
-		// Responsável por listar a transação.
-    app.get('/', async(request, reply) =>{  
+	// Responsável por listar a transação.
+    app.get('/', async(request, reply) =>{ 
+		
+		const transactions = await prisma.transaction.findMany()
+
+		// Retorna o array de transações dentro de um objeto como resposta
         reply.send({ transactions })
     })
-		// Responsável por listar uma transação específica.
+
+	// Responsável por listar uma transação específica.
     app.get('/:id', async(request, reply) =>{
 			// Define o esquema de validação para os parâmetros da rota
 			const getTransactionParamsSchema = z.object({
@@ -63,7 +66,11 @@ export async function transactionRoutes(app: FastifyInstance){
 			const { id } =getTransactionParamsSchema.parse(request.params)
 
 			// Busca a transação pelo id
-			const transaction = transactions.find((transaction) => transaction.id === Number(id))
+			const transaction = await prisma.transaction.findUnique({
+				where: {
+					id
+				}
+			})
 
 			// Se a transação não for encontrada, retorna um erro 404
 			if(!transaction) {
@@ -73,15 +80,15 @@ export async function transactionRoutes(app: FastifyInstance){
 			// Retorna a transação encontrada
 			return reply.send({ transaction })
     })
-		// Responsável por alterar uma transação específica
-		app.put('/:id', async(request, reply) =>{
+	// Responsável por alterar uma transação específica
+	app.put('/:id', async(request, reply) =>{
 			// Define o esquema de validação para os parâmetros da rota
-			const updateTransactionParamsSchema = z.object({
-				id: z.coerce.number()
-			})
+		const updateTransactionParamsSchema = z.object({
+			id: z.coerce.number()
+		})
 
-			// Define o esquema de validação para o corpo da requisição
-			const updateTransactionBodySchema = z.object({
+		// Define o esquema de validação para o corpo da requisição
+		const updateTransactionBodySchema = z.object({
 				title: z.string().min(1),
 				amount: z.number().positive(),
 				type:z.enum(['credit', 'debit']),
@@ -95,64 +102,123 @@ export async function transactionRoutes(app: FastifyInstance){
 			// Extrai os dados da transação do corpo da requisição e valida com o esquema
 			const { title, amount, type, category, paymentMethod } = updateTransactionBodySchema.parse(request.body)
 
-			// Encontra a transação com o ID fornecido
-			let transaction = transactions.find((transaction) => transaction.id === Number(id))
+			// Encontra a transação com o ID
+			const transaction = await prisma.transaction.findUnique({
+				where: {
+					id
+				}
+			})
 
 			// Se a transação não for encontrada, retorna um erro 404
 			if(!transaction){
 				return reply.status(404).send({ error: 'Transaction not found' })
 			}
 
-			//Atualiza os dados da transação
-			transaction.title = title;
-			transaction.amount = amount;
-			transaction.type = type;
-			transaction.category = category;
-			transaction.paymentMethod = paymentMethod;
+			//Atualiza a transação no banco de dados
+			await prisma.transaction.update({
+				where: {
+					id
+				},
+				data: {
+					title,
+					amount,
+					type,
+					category,
+					paymentMethod
+				},
+			})
 
 			// Retorna uma resposta com o código 200 (OK)
 			return reply.send()
 		})
-		// Responsável por deletar uma transação específica.
-		app.delete('/:id', async(request, reply) =>{
-			// Define o esquema de validação para os parâmetros da rota
-			const deleteTransactionParamsSchema = z.object({
-				id: z.coerce.number()
-			})
-			// Extrai o índice da transação dos parâmetros da rota da requisição e valida com o esquema
-			const { id } = deleteTransactionParamsSchema.parse(request.params)
-			// Encontra o índice da transação com o ID fornecido
-			let index = transactions.findIndex((transaction) => transaction.id === Number(id))
-			// Se a transação não for encontrada, retorna um erro 404
-			if (index === -1){
-				return reply.status(404).send({ error: 'Transaction not found' })
+
+	app.get('/transactions', async (request, reply) => {
+    	const getTransactionsQuerySchema = z.object({
+      	limit: z.coerce.number().min(10).max(100).default(10),
+      	page: z.coerce.number().min(1).default(1)
+    	})
+
+    	const { limit, page } = getTransactionsQuerySchema.parse(request.query)
+
+
+    	// Busca todas as transações no banco de dados
+    	const transactions = await prisma.transaction.findMany({
+      	orderBy: {
+        	id: 'desc'
+      	},
+      	take: limit,
+      	skip: (page - 1) * limit,
+    })
+
+    const totalTransactions = await prisma.transaction.count()
+
+    // Retorna o array de transações dentro de um objeto como resposta
+    return reply.send({ 
+      	transactions,
+      	page,
+      	limit,
+      	totalPages: Math.ceil(totalTransactions / limit)
+    	})
+  	})
+
+	// Responsável por deletar uma transação específica.
+	app.delete('/:id', async(request, reply) =>{
+		// Define o esquema de validação para os parâmetros da rota
+		const deleteTransactionParamsSchema = z.object({
+			id: z.coerce.number()
+		})
+
+		// Extrai o índice da transação dos parâmetros da rota da requisição e valida com o esquema
+		const { id } = deleteTransactionParamsSchema.parse(request.params)
+
+		// Deleta a transação do banco de dados
+		const transaction = await prisma.transaction.delete({
+			where: {
+				id
 			}
-			// Remove a transação do array
-			transactions.splice(index, 1)
-			// Retorna uma resposta de sucesso
-			return reply.status(204).send()
 		})
-		//	Responsável por retornar um resumo das transações.
-		app.get('/summary', async(request, reply)=>{
-			let totalCredit = 0
-			let totalDebit = 0
 
-			// Itera sobre todas as transações e acumula os valores de acordo com o tipo
-			transactions.forEach((transaction) => {
-				if(transaction.type === 'credit'){
-					totalCredit += transaction.amount
-				}else if (transaction.type === 'debit'){
-					totalDebit += transaction.amount
-				}
-			})
+		// Se a transação não for encontrada, retorna um erro 404
+		if(!transaction){
+			return reply.status(404).send({ error: 'Transaction not found' })
+		}
 
-			// Retorna o resumo das transações
-			return reply.send({
-				summary: {
-					totalCredit,
-					totalDebit,
-					netBalance: totalCredit - totalDebit
-				}
-			})
+		// Retorna uma resposta de sucesso
+		return reply.status(204).send() // Status 204 indica que a operação foi bem-sucedida sem conteúdo de retorno.
 		})
+	//	Responsável por retornar um resumo das transações.
+	app.get('/summary', async(request, reply)=>{
+		// Busca a soma de todas as transações de crédito
+		let aggregations = await prisma.transaction.aggregate({
+			_sum: {
+				amount: true,
+			},
+			where: {
+				type: 'credit'
+			},
+		})
+
+		const totalCredit = aggregations._sum.amount ?? 0
+
+		// Busca a soma de todas as transações de débito
+		aggregations = await prisma.transaction.aggregate({
+			_sum: {
+				amount: true,
+			},
+			where: {
+				type: 'debit'
+			},
+		})
+
+		const totalDebit = aggregations._sum.amount ?? 0
+
+		// Retorna o resumo das transações
+		return reply.send({
+			summary: {
+				totalCredit,
+				totalDebit,
+				netBalance: totalCredit - totalDebit
+			}
+		})
+	})
 }
